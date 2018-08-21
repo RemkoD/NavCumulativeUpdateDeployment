28ad57acb4b4b82e469bfc1f5056ad780dc6e990
Remko Dannenberg <remko3@gmail.com>
2018-08-21 18:57:56 +0200

    Update Load Deployment Settings.ps1
    Fixed wrong example in comment

--- Examples/Load Deployment Settings.ps1
+++ Examples/Load Deployment Settings.ps1
@@ -1,7 +1,7 @@
 ﻿# ********************************************** [Script Parameters] ********************************************** #
 
 # Set the NAV Release to patch with the cumulative platform update. 
-# Example: 'NAV2017'
+# Example: '2017'
     
     $NavVersion = ''
 
